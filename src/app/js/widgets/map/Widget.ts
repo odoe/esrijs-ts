49e32859ac55c2d451f482ec253689c916c81723
Rene Rubalcava <odoe@odoe.net>
2015-05-03 17:46:24 -0700

    remove Evented from Widget

--- src/app/js/widgets/map/Widget.ts
+++ src/app/js/widgets/map/Widget.ts
@@ -1,15 +1,13 @@
 import Map = require('esri/map');
 import dojoDeclare = require('dojo/_base/declare');
-import Evented = require('dojo/Evented');
 import _WidgetBase = require('dijit/_WidgetBase');
 import _TemplatedMixin = require('dijit/_TemplatedMixin');
 
-var Widget:any = dojoDeclare([_WidgetBase, _TemplatedMixin, Evented], {
+var Widget:any = dojoDeclare([_WidgetBase, _TemplatedMixin], {
   templateString: '<div></div>',
   postCreate() {
     var map:Map = new Map(this.domNode, this.get('mapOptions'));
     this.set('map', map);
-    this.emit('loaded', { map: map });
   }
 });
 
